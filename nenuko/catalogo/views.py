10ff4dc438e61ba1e4f52270313348ca3bc4ef01
unknown <nicolastatter45@gmail.com>
2020-11-05 03:22:35 -0300

    Nenuko Store: Creando Página de Inicio

--- nenuko/catalogo/views.py
+++ nenuko/catalogo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from . models import Manga, MangaInstance,Mangaka,Genre
+from django.views import generic
 # Create your views here.
+def index(request):
+    num_Mangas = Manga.objects.all().count()
+    num_Mangas_stock = MangaInstance.objects.filter(status__exact='s').count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_mangas': num_Mangas, 'num_mangas_stock': num_Mangas_stock},
+    )
+
+class MangaListView(generic.ListView):
+    model = Manga
+    paginate_by = 10
+class MangaDetailView(generic.DetailView):
+    model = Manga
