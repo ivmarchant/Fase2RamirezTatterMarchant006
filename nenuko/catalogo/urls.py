2cd437101059033171cc5e57cc467fa59eafd5eb
unknown <nicolastatter45@gmail.com>
2020-11-06 02:19:58 -0300

    Nenuko´s Store: Creando Formularios

--- nenuko/catalogo/urls.py
+++ nenuko/catalogo/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('mangas/', views.mangas, name='mangas'),
     path('figuras/', views.figuras, name='figuras'),
+    path('registro/', views.registro, name='registro'),
+    path('mangaka/<int:pk>', views.MangakaDetailView.as_view(), name='mangaka-detail'),
+]
+urlpatterns +=[
+    path('mangaka/create/', views.MangakaCreate.as_view(), name='mangaka_create'),
+    path('mangaka/<int:pk>/update/', views.MangakaUpdate.as_view(), name='mangaka_update'),
+    path('mangaka/<int:pk>/delete/', views.MangakaDelete.as_view(), name='mangaka_delete'),
 ]
--- nenuko/catalogo/views.py
+++ nenuko/catalogo/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from . models import Manga, MangaInstance,Mangaka,Genre
+from . models import Manga, MangaInstance, Mangaka, Genre
 from django.views import generic
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 # Create your views here.
+
 def index(request):
     num_Mangas = Manga.objects.all().count()
     num_Mangas_stock = MangaInstance.objects.filter(status__exact='s').count()
@@ -23,6 +28,25 @@ def figuras(request):
         request,
         'figuras.html',
     )
+def registro(request):
+    return render(
+        request,
+        'registro.html'
+    )
+class MangakaCreate(CreateView):
+    model = Mangaka
+    fields = '__all__'
+
+class MangakaUpdate(UpdateView):
+    model = Mangaka
+    fields = ['primer_nombre', 'apellido', 'fecha_nacimiento', 'fecha_muerte']
+
+class MangakaDelete(DeleteView):
+    model = Mangaka
+    success_url = reverse_lazy('index')
+
+class MangakaDetailView(generic.DetailView):
+    model=Mangaka
 
 class MangaListView(generic.ListView):
     model = Manga
